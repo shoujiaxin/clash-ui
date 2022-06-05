efaaa07b1e71cb7d5abace041b3cf1fe96dd2302
Jiaxin Shou <shoujiaxin@gmail.com>
2022-06-05 22:41:27 +0800

    feat: initial commit

---
+++ ClashUI/ClashUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClashUIApp.swift
+//  ClashUI
+//
+//  Created by Jiaxin Shou on 2022/6/5.
+//
+
+import SwiftUI
+
+@main
+struct ClashUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
