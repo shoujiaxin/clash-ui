0baf695d904677d16f854897a2c943a5409dddc7
Jiaxin Shou <shoujiaxin@gmail.com>
2022-06-11 23:25:46 +0800

    2

---
+++ ClashUI/Views/ConnectionsView.swift
@@ -0,0 +1,20 @@
+//
+//  ConnectionsView.swift
+//  ClashUI
+//
+//  Created by Jiaxin Shou on 2022/6/11.
+//
+
+import SwiftUI
+
+struct ConnectionsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ConnectionsView_Previews: PreviewProvider {
+    static var previews: some View {
+        ConnectionsView()
+    }
+}
